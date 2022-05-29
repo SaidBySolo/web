5f95f94d03382962e4fa0e9a18bb2348ca313b44
Ryu juheon <saidbysolo@gmail.com>
2022-05-30 00:00:04 +0900

    feat: bio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
+import { dark, CharcoalTheme } from '@charcoal-ui/theme'
+import createTheme from '@charcoal-ui/styled'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+declare module 'styled-components' {
+  export interface DefaultTheme extends CharcoalTheme { }
 }
 
-export default App;
+const theme = createTheme<CharcoalTheme>(styled)
+
+const Flex = styled.div`
+  display: flex;
+`
+
+const Center = styled(Flex)`
+  justify-content: center;
+  align-items: center;
+`
+
+const MainFlexBox = styled(Center)`
+  height: 100vh;
+  ${theme(o => [o.bg.background1])}
+`
+
+const InfoBox = styled(Flex)`
+  height: 50vh;
+  width: 80vh;
+  flex-direction: row;
+  @media screen and (max-width: 768px){
+    height: 80vh;
+    width: 50vh;
+    flex-direction: column;
+  }
+  ${theme(o => [o.bg.surface2, o.borderRadius(24)])}
+`
+
+const TextBox = styled(Flex)`
+  width: 70%;
+  order: 0;
+  @media screen and (max-width: 768px){
+    order: 1;
+  }
+  flex-direction: column;
+`
+
+const Heading = styled.h1`
+  @media screen and (max-width: 768px){
+    margin-top: 0;
+  }
+  ${theme(o => [o.font.text1, o.margin.horizontal(40), o.margin.top(40), o.margin.bottom(16)])}
+`
+
+const SubHeading = styled.h2`
+  ${theme(o => [o.font.text1, o.margin.horizontal(40), o.margin.top(0), o.margin.bottom(24)])}
+`
+
+const TextLink = styled.a`
+  ${theme(o => [o.font.text2, o.margin.horizontal(40), o.margin.top(0), o.margin.bottom(16)])}
+`
+
+const Profile = styled(Center)`
+  order: 1;
+  @media screen and (max-width: 768px){
+    order: 0;
+  }
+`
+
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  content:url("https://avatars.githubusercontent.com/u/33891531?v=4");
+  ${theme(o => [o.height.px(168), o.width.px(168), o.margin.all(40)])}
+`
+
+const App = () => (
+  <ThemeProvider theme={dark}>
+    <MainFlexBox>
+      <InfoBox>
+        <TextBox>
+          <Heading>
+            Ryu Juheon
+          </Heading>
+          <SubHeading>
+            a.k.a SaidBySolo
+          </SubHeading>
+          <TextLink href="https://github.com/SaidBySolo">
+            Github
+          </TextLink>
+          <TextLink href="https://twitter.com/SaidBySolo">
+            Twitter
+          </TextLink>
+          <TextLink href="https://discord.com/users/345265069132742657">
+            Discord
+          </TextLink>
+        </TextBox>
+        <Profile>
+          <ProfileImage alt="Profile image" />
+        </Profile>
+      </InfoBox>
+    </MainFlexBox>
+  </ThemeProvider >
+)
+export default App
